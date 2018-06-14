e3bb7c2769aabd3e99ab2ec5073f8adf2f8c849d
Ben Eckardt <ben.ec.00@gmail.com>
2018-06-14 16:47:13 +0200

    background flexgrid

--- public/main.js
+++ public/main.js
@@ -1,7 +1,12 @@
 function loadGames(){
-    var grid = document.getElementById("grid");
-    grid.innerHTML += '<a class="field" href="TicTacToe">TicTacToe</a>';
+    var grid = document.getElementById("linkList");
+    grid.innerHTML += '<a class="field" href="TicTacToe" style="background: url(\"tictactoe.jpg\") no-repeat center center;background-size:cover;">TicTacToe</a>';
     grid.innerHTML += '<a class="field" href="HappyBirthday">Happy Birthday</a>';
+}
+
+function changeBackground(element){
+    var source = element.style.backgroundImage;
+    document.body.children[0].style.backgroundImage = source;
 }
 
 window.onload = function(){
