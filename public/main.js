66e854e04c633621e02828c3600e6fcc6b5e6251
Ben Eckardt <ben.ec.00@gmail.com>
2018-06-14 14:03:06 +0200

    index

--- public/main.js
+++ public/main.js
@@ -1,3 +1,10 @@
-function openTicTacToe(){
-    
+function loadGames(){
+    var grid = document.getElementById("grid");
+    grid.innerHTML += '<a class="field" href="TicTacToe/tictactoe.html">TicTacToe</a>';
+    grid.innerHTML += '<a class="field" href="HappyBirthday/happybirthday.html">Happy Birthday</a>';
+}
+
+window.onload = function(){
+    console.log("hi");
+    loadGames();
 }
