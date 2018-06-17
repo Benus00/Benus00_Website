257f25beecaa88a6de60bb80b0cf779a75a5059b
Ben Eckardt <ben.ec.00@gmail.com>
2018-06-17 14:17:59 +0200

    wallpapers

--- public/main.js
+++ public/main.js
@@ -1,13 +1,14 @@
 function loadLinks(){
     var grid = document.getElementById("linkList");
-    grid.innerHTML += '<a class="field" href="TicTacToe">TicTacToe</a>';
-    grid.children[0].style.backgroundImage = 'url("tictactoe.jpg")';
-    grid.innerHTML += '<a class="field" href="HappyBirthday">Happy Birthday</a>';
-    grid.innerHTML += '<a class="field" href="4wins">4Wins(incomplete)</a>';
+    grid.innerHTML += '<a class="field" href="TicTacToe"> <p class="linkText">TicTacToe</p> </a>';
+    grid.children[0].style.backgroundImage = 'url("tictactoe.png")';
+    grid.innerHTML += '<a class="field" href="HappyBirthday"> <p class="linkText">Happy Birthday</p> </a>';
+    grid.innerHTML += '<a class="field" href="4wins"> <p class="linkText">4wins (incomplete)</p> </a>';
 }
 
 function changeBackground(element){
     var source = element.style.backgroundImage;
+    source = source.replace("/Small", "");
     document.body.children[0].style.backgroundImage = source;
     console.log(document.getElementsByClassName("field"));
 }
