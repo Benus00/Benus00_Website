6852158b59c2b3e9c3ac3ed429a3371aad522504
Ben Eckardt <ben.ec.00@gmail.com>
2018-06-26 20:09:19 +0200

    gallery

---
+++ public/Gallery/main.js
@@ -0,0 +1,106 @@
+var backgroundListData = []
+var diashowTimer;
+
+function loadJSON(path, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            backgroundListData = JSON.parse(xobj.responseText);
+            callback(backgroundListData);
+        }
+    };
+    xobj.send(null);
+}
+
+function loadImages(jsonData){
+    var templateImage = '<div id="backgroundImage#id#" class="backgroundOption" style=\'background-image: url("' + window.location.href.slice(0, -8) + 'Wallpapers/Small/#name##format#")\' onclick="changeBackgroundFromElement(this)"></div>';
+    var imageList = document.getElementById('backgroundList');
+
+    jsonData.forEach(element => {
+        var htmlElement =  templateImage.replace('#id#', element.id).replace('#name#', element.name).replace('#format#', element.fileformat)
+        imageList.innerHTML += htmlElement;
+    });
+    changeBackground("book-autumn", ".jpg");
+}
+
+function changeBackgroundFromElement(element){
+    var source = element.style.backgroundImage;
+    // console.log(source);
+    sourceName = imageNameByUrl(source);
+    sourceDataFormat = imageDataFormatByUrl(source);
+    changeBackground(sourceName, sourceDataFormat);
+}
+
+function changeBackground(name, fileformat){
+    // console.log('url("' + window.location.href.slice(0, -8) + 'Wallpapers/' + name + fileformat + '")');
+    var source = 'url("' + window.location.href.slice(0, -8) + 'Wallpapers/' + name + fileformat + '")';
+    document.body.children[0].style.backgroundImage = source;
+}
+
+function toggleDiashow(element){
+    if(element.checked == true){
+        diashowTimer = window.setInterval(function(){nextBackground(-1)}, 3000);
+    }
+    else{
+        window.clearInterval(diashowTimer);
+    }
+}
+
+function nextBackground(count = 1){
+    var oldName = document.body.children[0].style.backgroundImage;
+    oldName = imageNameByUrl(oldName);
+    var oldImageData = imageDataByName(oldName);
+    var newImageId = Number(oldImageData.id)+count;
+    while(newImageId>backgroundListData.length){
+        newImageId -= backgroundListData.length;
+    }
+    while(newImageId<=0){
+        newImageId += backgroundListData.length;
+    }
+    newImageData = imageDataById(newImageId);
+    changeBackground(newImageData.name, newImageData.fileformat);
+}
+
+function imageNameByUrl(url){
+    var name = url;
+    if(url.indexOf("url(") == 0){
+        name = name.slice(5, -2);
+    }
+    name = name.split("/");
+    name = name.pop().slice(0, -4);
+    return name;
+}
+
+function imageDataFormatByUrl(url){
+    var name = url;
+    if(url.indexOf("url(") == 0){
+        name = name.slice(5, -2);
+    }
+    name = name.split("/");
+    name = name.pop().slice(-4);
+    return name;
+}
+
+function imageDataByName(name){
+    for(var i=0; i<backgroundListData.length; i++){
+        if(backgroundListData[i].name == name){
+            return backgroundListData[i];
+        }
+    }
+}
+
+function imageDataById(id){
+    for(var i=0; i<backgroundListData.length; i++){
+        if(backgroundListData[i].id == id){
+            return backgroundListData[i];
+        }
+    }
+}
+
+window.onload = function(){
+    // console.log(window.location.href.slice(0, -8) + 'Wallpapers/wallpapers.json');
+    loadJSON(window.location.href.slice(0, -8) + 'Wallpapers/wallpapers.json', loadImages);
+}
--- public/main.js
+++ public/main.js
@@ -1,6 +1,3 @@
-var backgroundListData = []
-var diashowTimer;
-
 function loadJSON(path, callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
@@ -54,67 +51,6 @@ function loadLinks(){
     // list.innerHTML += '<a class="field" href="Gallery"> <p class="linkText">Gallery</p> </a>';
 }
 
-function loadImages(jsonData){
-    var templateImage = '<div id="backgroundImage#id#" class="backgroundOption" style=\'background-image: url("Wallpapers/Small/#name##format#")\' onclick="changeBackgroundFromElement(this)"></div>';
-    var imageList = document.getElementById('backgroundList');
-
-    jsonData.forEach(element => {
-        var htmlElement =  templateImage.replace('#id#', element.id).replace('#name#', element.name).replace('#format#', element.fileformat)
-        imageList.innerHTML += htmlElement;
-    });
-    changeBackground("fantasy-woman-castle", ".jpg");
-}
-
-function changeBackgroundFromElement(element){
-    var source = element.style.backgroundImage;
-    sourceName = source.slice(22, source.length-6);
-    sourceDataFormat = source.slice(source.length-6, source.length-2);
-    changeBackground(sourceName, sourceDataFormat);
-}
-
-function changeBackground(name, fileformat){
-    var source = 'url("Wallpapers/' + name + fileformat + '")';
-    document.body.children[0].style.backgroundImage = source;
-}
-
-function toggleDiashow(element){
-    if(element.checked == true){
-        diashowTimer = window.setInterval(nextBackground, 10000);
-    }
-    else{
-        window.clearInterval(diashowTimer);
-    }
-}
-
-function nextBackground(){
-    var oldName = document.body.children[0].style.backgroundImage;
-    oldName = oldName.slice(16, oldName.length-6);
-    var oldImageData = imageDataByName(oldName);
-    var newImageId = Number(oldImageData.id)+1;
-    if(newImageId>backgroundListData.length){
-        newImageId = 1;
-    }
-    newImageData = imageDataById(newImageId);
-    changeBackground(newImageData.name, newImageData.fileformat);
-}
-
-function imageDataByName(name){
-    for(var i=0; i<backgroundListData.length; i++){
-        if(backgroundListData[i].name == name){
-            return backgroundListData[i];
-        }
-    }
-}
-
-function imageDataById(id){
-    for(var i=0; i<backgroundListData.length; i++){
-        if(backgroundListData[i].id == id){
-            return backgroundListData[i];
-        }
-    }
-}
-
 window.onload = function(){
     loadLinks();
-    loadJSON('Wallpapers/wallpapers.json', loadImages);
 }
