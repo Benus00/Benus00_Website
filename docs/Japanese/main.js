6fd65264759a12a1f8912a961def6cdf9e7dd8ee
Ben Eckardt <31539307+Benus00@users.noreply.github.com>
2019-03-27 22:58:54 +0100

    japanese image

--- docs/Japanese/main.js
+++ docs/Japanese/main.js
@@ -1,7 +1,10 @@
 function backImage(){
-    console.log("afds");
+    element = document.getElementsByClassName("image")[0];
+    element.style.backgroundImage = (parseInt(element.style.backgroundImage.slice(7, -3))-1)+".jpg";
 }
 
 function forwardImage(){
-    console.log("afds");
+    element = document.getElementsByClassName("image")[0];
+    console.log(JSON.stringify(element)+element.style.backgroundImage+".jpg");
+    element.style.backgroundImage = (parseInt(element.style.backgroundImage.slice(7, -3))+1)+".jpg";
 }
